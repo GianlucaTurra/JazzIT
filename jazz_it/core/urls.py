c0b493b56c6f7f284dc4516f246a634cf9290e3f
Gianluca Turra <gianlucaturra1@gmail.com>
2024-09-15 22:16:33 +0200

    First try to implement the add form

--- jazz_it/core/urls.py
+++ jazz_it/core/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('login/', LoginView.as_view(template_name='core/login.html', authentication_form=LoginForm), name='login'),
     path('logout/', views.logout_user, name='logout'),
+    path('add_ma/', views.add_music_advice, name='add_music_advice'),
 ]
